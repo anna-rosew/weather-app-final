444da30bce3ae02a569d4bf07845362a92d5dcfd
Anna <annarosewain9@gmail.com>
2024-02-14 11:57:46 +0000

    Layout of weekly forecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,18 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  <div className="WeatherForecast">
+    <div className="row">
+      <div className="col">
+        <div className="WeatherForecast-day">Thurs</div>
+        <WeatherIcon code="09d" size={36} />
+        <div className="WeatherForecast-temp">
+          <span className="WeatherForecast-temp-max">12°</span>
+          <span className="WeatherForecast-temp-max">10°</span>
+        </div>
+      </div>
+    </div>
+  </div>;
+}
