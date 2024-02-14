53c94a1287eba15910297d523fea13905eba1266
Anna <annarosewain9@gmail.com>
2024-02-14 12:00:39 +0000

    Bug fix

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,16 +3,18 @@ import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
-  <div className="WeatherForecast">
-    <div className="row">
-      <div className="col">
-        <div className="WeatherForecast-day">Thurs</div>
-        <WeatherIcon code="09d" size={36} />
-        <div className="WeatherForecast-temp">
-          <span className="WeatherForecast-temp-max">12°</span>
-          <span className="WeatherForecast-temp-max">10°</span>
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thurs</div>
+          <WeatherIcon code="09d" size={36} />
+          <div className="WeatherForecast-temp">
+            <span className="WeatherForecast-temp-max">12°</span>
+            <span className="WeatherForecast-temp-max">10°</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 }
